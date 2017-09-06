38496ac256677ffce030908567752931ff307f2b
stackinger <705892070@qq.com>
2017-09-07 00:10:46 +0800

    搜索用户页面布局以及跳转事件

---
+++ app/src/main/java/com/example/zhouk/footballsupporter/UserDetailActivity.java
@@ -0,0 +1,60 @@
+package com.example.zhouk.footballsupporter;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.zhouk.footballsupporter.base.BaseActivity;
+
+public class UserDetailActivity extends BaseActivity {
+    private final int requestCode = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_detail);
+
+        setTitle("个人资料");
+        setBackBtn();
+
+        toUserList();
+
+    }
+
+    /*按钮添加事件*/
+    protected void toUserList() {
+        Button button = (Button) findViewById(R.id.ud_delBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(UserDetailActivity.this).setTitle("系统提示")//设置对话框标题
+                        .setMessage("是否确认删除用户？")//设置显示的内容
+                        .setPositiveButton("确定",new DialogInterface.OnClickListener() {//添加确定按钮
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {//确定按钮的响应事件
+                                // TODO Auto-generated method stub
+                                Toast.makeText(getApplicationContext(),"已删除该用户！",Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        }).setNegativeButton("返回",new DialogInterface.OnClickListener() {//添加返回按钮
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {//响应事件
+                        // TODO Auto-generated method stub
+                        /*Log.i("alertdialog"," 请保存数据！");*/
+                    }
+                }).show();//在按键响应事件中显示此对话框
+
+
+                /*Intent intent = new Intent(UserDetailActivity.this, OrderPingJiaActivity.class);
+                //采用Intent普通传值的方式
+                intent.putExtra("skip", "我是OrderDetailActivity传过来的值！");
+                //跳转Activity
+                startActivityForResult(intent, requestCode);*/
+            }
+        });
+    }
+}
